aab1dbae43165eaf622bab6cd9b62a6c2fd2ad83
noka nguyen <noka.hieunt@gmail.com>
2021-06-01 22:52:03 +0700

    first commit

---
+++ src/main/java/com/nokavietnam/springmssql/SpringMssqlApplication.java
@@ -0,0 +1,13 @@
+package com.nokavietnam.springmssql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMssqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMssqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nokavietnam/springmssql/SpringMssqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokavietnam.springmssql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMssqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
